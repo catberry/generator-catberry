99af8c9bfd81ea6b9b4ffed8e00500a7112d9075
Sasha Go <a@ptx.su>
2015-06-02 19:00:07 +0600

    #2 done

--- component/index.js
+++ component/index.js
@@ -8,6 +8,11 @@ module.exports = yg.NamedBase.extend({
         this.tpl = tpls.tpls[this.config.get('tpl') || tpls.default];
     },
     writing: function () {
-        this.log(this.tpl);
+        var TO = this.destinationPath('catberry_components/' + this.name + '/');
+        var template = 'template' + this.tpl.suffix;
+        var catComponent = {template: template};
+        this.fs.writeJSON(TO + 'cat-component.json', catComponent);
+        this.fs.write(TO + template, ".");
+        this.fs.copy(this.templatePath('index.js'), TO + 'index.js');
     }
 });
