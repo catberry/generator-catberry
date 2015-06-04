e26567261fe8e7d670dbb47eb3dfcbcdd80fb60a
Sasha Go <a@ptx.su>
2015-06-04 23:10:34 +0600

    #5: this.ctx introduced

--- app/index.js
+++ app/index.js
@@ -10,29 +10,32 @@ module.exports = yg.Base.extend({
             desc: 'Template engine to use'
         });
         var tpl = this.options.tpl;
-        this.te = cfg.tes[tpl]; //TODO: cfg.tes[tpl]?
+        var te = cfg.tes[tpl]; //TODO: cfg.tes[tpl]?
+        this.ctx = {te: te};
         this.config.set({tpl: tpl});
     },
     //see issue #5
     writing: function () {
+        var ctx = this.ctx;
         var from = this.sourceRoot() + '/';
         var to = this.destinationRoot() + '/';
         this.fs.copy(from + '_gitignore', to + '.gitignore');
-        this.fs.copyTpl(from + 'gcat.js', to + 'gcat.js', {te: this.te});
+        this.fs.copyTpl(from + 'gcat.js', to + 'gcat.js', ctx);
         this.fs.copy(from + '{browser.js,build.js,package.json,README.md,routes.js,server.js}', to);
         // document
         this.fs.copy(from + 'catberry_components/document/index.js', to + 'catberry_components/document/index.js');
-        this.fs.copy(from + 'catberry_components/document/template' + this.te.suffix, to + 'catberry_components/document/template' + this.te.suffix);
-        this.fs.writeJSON(to + 'catberry_components/document/cat-component.json', {template: 'template' + this.te.suffix});
+        this.fs.copy(from + 'catberry_components/document/template' + ctx.te.suffix, to + 'catberry_components/document/template' + ctx.te.suffix);
+        this.fs.writeJSON(to + 'catberry_components/document/cat-component.json', {template: 'template' + ctx.te.suffix});
         // head
         this.fs.copy(from + 'catberry_components/head/index.js', to + 'catberry_components/head/index.js');
-        this.fs.copy(from + 'catberry_components/head/template' + this.te.suffix, to + 'catberry_components/head/template' + this.te.suffix);
-        this.fs.writeJSON(to + 'catberry_components/head/cat-component.json', {template: 'template' + this.te.suffix});
+        this.fs.copy(from + 'catberry_components/head/template' + ctx.te.suffix, to + 'catberry_components/head/template' + ctx.te.suffix);
+        this.fs.writeJSON(to + 'catberry_components/head/cat-component.json', {template: 'template' + ctx.te.suffix});
     },
     install: function () {
+        var ctx = this.ctx;
         var npmDeps = [
             'connect', 'serve-static', 'errorhandler',
-            'catberry', this.te.package
+            'catberry', ctx.te.package
         ];
         this.npmInstall(npmDeps, {save: true});
     }
