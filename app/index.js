66bab7a1ea12c6f1eb95791e5f5ed62a897aec07
Sasha Go <a@ptx.su>
2015-06-09 01:39:22 +0600

    cfg made global

--- app/index.js
+++ app/index.js
@@ -1,9 +1,9 @@
-var yg = require('yeoman-generator');
+var yg = require('yeoman-generator'),
+    cfg = require('../cfg.json');
 
 module.exports = yg.Base.extend({
     constructor: function () {
         yg.Base.apply(this, arguments);
-        var cfg = this.cfg = require('../cfg.json');
         this.option('tpl', {
             type: String,
             defaults: Object.keys(cfg.tes)[0],
@@ -18,16 +18,16 @@ module.exports = yg.Base.extend({
         //
         this.fs.copy(SRC + '_gitignore', DEST + '.gitignore');
         this.fs.copy(SRC + '{browser.js,build.js,package.json,README.md,routes.js,server.js}', DEST);
-        this.fs.copyTpl(SRC + 'gcat.js', DEST + 'gcat.js', {te: this.cfg.tes[this.config.get('tpl')]});
+        this.fs.copyTpl(SRC + 'gcat.js', DEST + 'gcat.js', {te: cfg.tes[this.config.get('tpl')]});
         this._generateComponents();
     },
     _generateComponents: function () {
         //
-        var SRC = this.sourceRoot() + '/' + this.cfg.dirs.components + '/';
-        var DEST = this.destinationRoot() + '/' + this.cfg.dirs.components + '/';
-        var logic = this.cfg.component.logic;
-        var template = this.cfg.component.template_ + this.cfg.tes[this.config.get('tpl')].suffix;
-        var descriptor = this.cfg.component.descriptor;
+        var SRC = this.sourceRoot() + '/' + cfg.dirs.components + '/';
+        var DEST = this.destinationRoot() + '/' + cfg.dirs.components + '/';
+        var logic = cfg.component.logic;
+        var template = cfg.component.template_ + cfg.tes[this.config.get('tpl')].suffix;
+        var descriptor = cfg.component.descriptor;
         var catComponent = {template: template};
         // document
         this.fs.copy(SRC + 'document/' + logic, DEST + 'document/' + logic);
@@ -41,7 +41,7 @@ module.exports = yg.Base.extend({
     install: function () {
         var npmDeps = [
             'connect', 'serve-static', 'errorhandler',
-            'catberry', this.cfg.tes[this.config.get('tpl')].package
+            'catberry', cfg.tes[this.config.get('tpl')].package
         ];
         this.npmInstall(npmDeps, {save: true});
     }
