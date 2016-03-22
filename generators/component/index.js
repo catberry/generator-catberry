2eb0cb96404385fb5bee2360bf27e1e82b6668a9
Sasha Go <golubev@fastmail.uk>
2016-03-22 22:25:51 +0600

    Prepare for catberry module presets

--- generators/component/index.js
+++ generators/component/index.js
@@ -1,7 +1,5 @@
 'use strict';
 var yeoman = require('yeoman-generator');
-var chalk = require('chalk');
-var yosay = require('yosay');
 
 module.exports = yeoman.generators.Base.extend({
 
@@ -11,29 +9,9 @@ module.exports = yeoman.generators.Base.extend({
     this.option('preset', {type: String, defaults: 'handlebars'});
   },
 
-  prompting: function () {
-    this.log(yosay(
-      'Welcome to the classy ' + chalk.red('generator-catberry') + ' generator!'
-    ));
-    var prompts = [];
-    var done = this.async();
-    this.prompt(prompts, function (props) {
-      // To access props later use this.props.someOption;
-      this.props = props;
-      done();
-    }.bind(this));
-  },
-
   writing: function () {
-    // this.fs.copy(
-    //   this.templatePath('dummyfile.txt'),
-    //   this.destinationPath('dummyfile.txt')
-    // );
-    this.spawnCommandSync(this.sourceRoot() + '/../../../node_modules/.bin/catberry', ['addcomp', '--preset', this.options.preset, this.componentName]);
-  },
-
-  install: function () {
-    // this.installDependencies();
+    // this.spawnCommandSync(this.sourceRoot() + '/../../../node_modules/.bin/catberry',
+    //   ['addcomp', '--preset', this.options.preset, this.componentName]);
   }
 
 });
