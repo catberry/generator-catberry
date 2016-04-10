ed502f847939e842df97e02dbee2f444e57a8c10
Sasha Go <golubev@fastmail.uk>
2016-04-11 00:05:27 +0700

    #12 Deal with app name

--- generators/app/index.js
+++ generators/app/index.js
@@ -7,10 +7,11 @@ module.exports = BaseGenerator.extend({
   constructor: function () {
     BaseGenerator.apply(this, arguments);
     this.option('preset', {desc: 'App preset', alias: 'p', defaults: 'empty-handlebars'});
-    this.argument('appName', {desc: 'App name', required: false});
+    this.argument('appName', {desc: 'App name', required: false, defaults: this.appname});
   },
 
   writing: function () {
+    this.log('App name = ' + this.appName);
     this.fs.copy(
       this.templatePath(this.options.preset + '/**/*'),
       this.destinationRoot()
