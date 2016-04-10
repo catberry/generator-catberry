e3e50cea91c4a6e214e73942c9d613a1b0c19e4c
Sasha Go <golubev@fastmail.uk>
2016-04-11 03:19:50 +0700

    No empty-

--- generators/app/index.js
+++ generators/app/index.js
@@ -6,7 +6,7 @@ module.exports = BaseGenerator.extend({
 
   constructor: function () {
     BaseGenerator.apply(this, arguments);
-    this.option('preset', {desc: 'App preset', alias: 'p', defaults: 'empty-handlebars'});
+    this.option('preset', {desc: 'App preset', alias: 'p', defaults: 'handlebars'});
     this.argument('appName', {desc: 'App name', required: false, defaults: this.appname});
   },
 
